fc4b08571d34f5db9d51fcfa78da2c55e5b48ef9
nekkan <nxkkdev@gmail.com>
2021-03-14 20:12:02 -0300

    Add Kord dependency to interaction declarations

--- interaction-declarations/build.gradle.kts
+++ interaction-declarations/build.gradle.kts
@@ -8,11 +8,13 @@ group = "net.perfectdreams.discordinteraktions"
 version = "0.0.1-SNAPSHOT"
 
 repositories {
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
     mavenCentral()
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+    api("dev.kord:kord-rest:0.7.0-SNAPSHOT")
 }
 
 tasks.test {
