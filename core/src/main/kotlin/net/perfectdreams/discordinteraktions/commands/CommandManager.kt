adcf12a6863a01192b85990a07045663b5af93e0
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-04-20 21:24:05 -0300

    Add global command creation

--- core/src/main/kotlin/net/perfectdreams/discordinteraktions/commands/CommandManager.kt
+++ core/src/main/kotlin/net/perfectdreams/discordinteraktions/commands/CommandManager.kt
@@ -78,7 +78,7 @@ class CommandManager(
         )
     }
 
-    /* suspend fun updateAllGlobalCommands(
+    suspend fun updateAllGlobalCommands(
         deleteUnknownCommands: Boolean = true
     ) {
         if (deleteUnknownCommands) {
@@ -89,19 +89,17 @@ class CommandManager(
 
             // This will remove all commands that do not match any label of the currently registered commands
             // We don't need to remove commands that have the same label because the PUT request will replace them automatically
-            val allCommandsLabels = commands.map { it.rootDeclaration.name }
+            val allCommandsLabels = currentlyRegisteredCommands.map { it.name }
+            val allDeclaredLabels = declarations.map { it.name }
 
-            val commandsToBeRemoved = commands
-                .filter {
-                    it.rootDeclaration.name !in allCommandsLabels
-                }
+            val commandsToBeRemoved = allCommandsLabels - allDeclaredLabels
 
             commandsToBeRemoved.forEach {
                 val command = currentlyRegisteredCommands.first { registeredCommand ->
-                    registeredCommand.name == it.rootDeclaration.name
+                    registeredCommand.name == it
                 }
 
-                logger.info { "Deleting command ${it.rootDeclaration.name} (${command.id}) because there isn't any matching registered command..." }
+                logger.info { "Deleting command $it (${command.id}) because there isn't any matching registered command..." }
 
                 rest.interaction.deleteGlobalApplicationCommand(
                     applicationId,
@@ -112,16 +110,15 @@ class CommandManager(
 
         val commandsToBeRegistered = mutableListOf<ApplicationCommandCreateRequest>()
 
-        // If there are multiple commands, we need to do a distinct check based on the root declaration
-        for (slash in commands.distinctBy { it.rootDeclaration }) {
-            commandsToBeRegistered.add(convertDeclarationToKord(slash.rootDeclaration))
+        for (slash in declarations) {
+            commandsToBeRegistered.add(convertDeclarationToKord(slash))
         }
 
         rest.interaction.createGlobalApplicationCommands(
             applicationId,
             commandsToBeRegistered
         )
-    } */
+    }
 
     private fun convertDeclarationToKordSubCommandOption(declaration: SlashCommandDeclarationBuilder): ApplicationCommandOption {
         return ApplicationCommandOption(
