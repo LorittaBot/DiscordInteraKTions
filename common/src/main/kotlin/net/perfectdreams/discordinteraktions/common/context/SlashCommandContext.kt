e37866917d8f20a1ffade7d3f8e725af196dfae3
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-06-27 11:33:56 -0300

    Smol refactors

--- common/src/main/kotlin/net/perfectdreams/discordinteraktions/common/context/SlashCommandContext.kt
+++ common/src/main/kotlin/net/perfectdreams/discordinteraktions/common/context/SlashCommandContext.kt
@@ -7,16 +7,12 @@ import net.perfectdreams.discordinteraktions.common.utils.MessageBuilder
 import net.perfectdreams.discordinteraktions.common.utils.buildMessage
 
 open class SlashCommandContext(
-    // TODO:
-    // val request: CommandInteraction,
-    // val relativeOptions: List<Option>?,
     internal var bridge: RequestBridge,
     val sender: User
 ) {
     val isDeferred
         get() = bridge.state.value != InteractionRequestState.NOT_REPLIED_YET
 
-    // val user: User = KordUser(request.member.value?.user?.value ?: request.user.value ?: throw IllegalArgumentException("There isn't a user object present! Discord Bug?"))
     private var wasInitiallyDeferredEphemerally = false
 
     suspend fun defer(isEphemeral: Boolean) {
@@ -35,7 +31,6 @@ open class SlashCommandContext(
         if (message.files?.isNotEmpty() == true && !isDeferred) {
             // If the message has files and our current bridge state is "NOT_REPLIED_YET", then it means that we need to defer before sending the file!
             // (Because currently you can only send files by editing the original interaction message or with a follow up message
-            // TODO: Check if the ephemeral flag is set to true, you can't send files in a ephemeral message!
             defer(false)
         }
 
@@ -51,13 +46,4 @@ open class SlashCommandContext(
 
         return bridge.manager.sendMessage(theRealMessageThatWillBeSent)
     }
-
-    // TODO:
-    /* suspend fun sendEphemeralMessage(block: EphemeralMessageBuilder.() -> (Unit)) = sendMessage {
-        val ephemeralMessage = EphemeralMessageBuilder().apply(block)
-
-        content = ephemeralMessage.content
-        allowedMentions = ephemeralMessage.allowedMentions
-        flags = MessageFlags(MessageFlag.Ephemeral)
-    } */
 }
