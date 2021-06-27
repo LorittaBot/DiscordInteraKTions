b7b6de96627c0d6b1edd7c3b03b1478326fbc564
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-06-27 10:48:04 -0300

    Remove unused command context (for now)

--- platforms/gateway-jda/src/main/kotlin/net/perfectdreams/discordinteraktions/platform/jda/context/JDASlashCommandContext.kt
+++
@@ -1,6 +0,0 @@
-package net.perfectdreams.discordinteraktions.platform.jda.context
-
-import net.perfectdreams.discordinteraktions.common.context.RequestBridge
-import net.perfectdreams.discordinteraktions.common.context.SlashCommandContext
-
-class JDASlashCommandContext(bridge: RequestBridge) : SlashCommandContext(bridge)
