abcef369cd38970e286855aec71b88164b888ef5
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-08-18 14:46:26 -0300

    Remove core folder

--- core/src/main/kotlin/net/perfectdreams/discordinteraktions/internal/entities/KordGuildChannel.kt
+++
@@ -1,9 +0,0 @@
-package net.perfectdreams.discordinteraktions.internal.entities
-
-import dev.kord.common.entity.DiscordChannel
-import net.perfectdreams.discordinteraktions.api.entities.GuildChannel
-
-class KordGuildChannel(handle: DiscordChannel) : GuildChannel, KordChannel(handle) {
-    override val name: String
-        get() = handle.name.value!!
-}
--- core/src/main/kotlin/net/perfectdreams/discordinteraktions/utils/EphemeralMessageBuilder.kt
+++
@@ -1,8 +0,0 @@
-package net.perfectdreams.discordinteraktions.utils
-
-import dev.kord.common.entity.AllowedMentions
-
-class EphemeralMessageBuilder {
-    var content: String? = null
-    var allowedMentions: AllowedMentions? = null
-}
