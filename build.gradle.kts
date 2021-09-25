c0c436e0da7ec1995ef932cfa13501830ed319a0
MrPowerGamerBR <git@mrpowergamerbr.com>
2021-09-25 16:09:10 -0300

    0.0.10-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "net.perfectdreams.discordinteraktions"
-version = "0.0.9"
+version = "0.0.10-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ allprojects {
 
 subprojects {
     apply<MavenPublishPlugin>()
-    version = "0.0.9"
+    version = "0.0.10-SNAPSHOT"
 
     publishing {
         repositories {
