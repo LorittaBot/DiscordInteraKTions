d379a9bcec876951f8a64f7823b5250254d5a8fe
onlyfx <nonlyfx@gmail.com>
2021-11-06 19:06:37 -0300

    Changed guild to non-private

--- platforms/common-kord/src/main/kotlin/net/perfectdreams/discordinteraktions/platforms/kord/entities/KordGuild.kt
+++ platforms/common-kord/src/main/kotlin/net/perfectdreams/discordinteraktions/platforms/kord/entities/KordGuild.kt
@@ -4,6 +4,6 @@ import dev.kord.common.entity.DiscordGuild
 import dev.kord.common.entity.Snowflake
 import net.perfectdreams.discordinteraktions.api.entities.Guild
 
-class KordGuild(private val guild: DiscordGuild) : Guild {
+class KordGuild(val guild: DiscordGuild) : Guild {
     override val id = guild.id
 }
