325b23dc5445a3e32feb7744e671c8a0e4d27be9
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-15 16:09:38 -0300

    Version 0.0.11

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val DISCORD_INTERAKTIONS = "0.0.11-SNAPSHOT"
+    const val DISCORD_INTERAKTIONS = "0.0.11"
     const val KOTLIN = "1.6.10"
     const val KORD = "bugfix-autocomplete-serialization-SNAPSHOT" // 0.8.x-SNAPSHOT
     const val KTOR = "1.6.7"
