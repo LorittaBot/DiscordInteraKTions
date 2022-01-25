37e9e2230bb3a5e0318da5c219b95e4bb4bad9ab
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-01-25 11:17:52 -0300

    Update Kord

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val DISCORD_INTERAKTIONS = "0.0.12-SNAPSHOT"
     const val KOTLIN = "1.6.10"
-    const val KORD = "bugfix-autocomplete-serialization-SNAPSHOT" // 0.8.x-SNAPSHOT
+    const val KORD = "0.8.x-SNAPSHOT"
     const val KTOR = "1.6.7"
 }
