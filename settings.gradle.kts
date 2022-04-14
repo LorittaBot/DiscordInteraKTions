5fc5fbf9275ba28b338cfeb83d8d3ac62045aee0
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-04-14 19:50:38 -0300

    Update from Ktor 2.0.0 Beta to Ktor 2.0.0
    Thanks DiamondMiner88

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
             alias("kord-common").to("dev.kord", "kord-common").versionRef("kord")
             alias("kord-rest").to("dev.kord", "kord-rest").versionRef("kord")
             alias("kord-gateway").to("dev.kord", "kord-gateway").versionRef("kord")
-            alias("ktor-server-netty").to("io.ktor:ktor-server-netty:2.0.0-beta-1")
+            alias("ktor-server-netty").to("io.ktor:ktor-server-netty:2.0.0")
         }
     }
 }
