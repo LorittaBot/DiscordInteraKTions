eb735f6326b4a8bdf3c7bf40365a7f0259845717
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-04-14 19:11:09 -0300

    Fix compilation

--- common/src/main/kotlin/net/perfectdreams/discordinteraktions/platforms/kord/utils/KordAutocompleteChecker.kt
+++ common/src/main/kotlin/net/perfectdreams/discordinteraktions/platforms/kord/utils/KordAutocompleteChecker.kt
@@ -3,8 +3,8 @@ package net.perfectdreams.discordinteraktions.platforms.kord.utils
 import dev.kord.common.entity.Choice
 import dev.kord.common.entity.CommandArgument
 import dev.kord.common.entity.DiscordInteraction
-import dev.kord.rest.builder.interaction.IntChoiceBuilder
-import dev.kord.rest.builder.interaction.NumberChoiceBuilder
+import dev.kord.rest.builder.interaction.IntegerOptionBuilder
+import dev.kord.rest.builder.interaction.NumberOptionBuilder
 import dev.kord.rest.builder.interaction.StringChoiceBuilder
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -13,20 +13,12 @@ import net.perfectdreams.discordinteraktions.common.autocomplete.AutocompleteCon
 import net.perfectdreams.discordinteraktions.common.autocomplete.AutocompleteExecutor
 import net.perfectdreams.discordinteraktions.common.autocomplete.FocusedCommandOption
 import net.perfectdreams.discordinteraktions.common.autocomplete.GuildAutocompleteContext
-import net.perfectdreams.discordinteraktions.common.commands.ApplicationCommandContext
 import net.perfectdreams.discordinteraktions.common.commands.CommandManager
-import net.perfectdreams.discordinteraktions.common.commands.GuildApplicationCommandContext
 import net.perfectdreams.discordinteraktions.common.commands.SlashCommandDeclaration
-import net.perfectdreams.discordinteraktions.common.commands.options.ChoiceableCommandOption
+import net.perfectdreams.discordinteraktions.common.commands.options.*
+import net.perfectdreams.discordinteraktions.common.interactions.InteractionData
 import net.perfectdreams.discordinteraktions.common.requests.managers.RequestManager
 import net.perfectdreams.discordinteraktions.common.utils.InteraKTionsExceptions
-import net.perfectdreams.discordinteraktions.common.commands.options.IntegerCommandOption
-import net.perfectdreams.discordinteraktions.common.commands.options.NullableIntegerCommandOption
-import net.perfectdreams.discordinteraktions.common.commands.options.NullableNumberCommandOption
-import net.perfectdreams.discordinteraktions.common.commands.options.NullableStringCommandOption
-import net.perfectdreams.discordinteraktions.common.commands.options.NumberCommandOption
-import net.perfectdreams.discordinteraktions.common.commands.options.StringCommandOption
-import net.perfectdreams.discordinteraktions.common.interactions.InteractionData
 import net.perfectdreams.discordinteraktions.platforms.kord.commands.CommandDeclarationUtils
 import net.perfectdreams.discordinteraktions.platforms.kord.entities.KordInteractionMember
 import net.perfectdreams.discordinteraktions.platforms.kord.entities.KordUser
@@ -128,7 +120,7 @@ class KordAutocompleteChecker(val commandManager: CommandManager) {
                     autocompleteExecutor as AutocompleteExecutor<Long>
                     val autocompleteResult = autocompleteExecutor.onAutocomplete(autocompleteContext, focusedCommandOption)
                     bridge.manager.sendIntegerAutocomplete(
-                        (IntChoiceBuilder("<auto-complete>", "")
+                        (IntegerOptionBuilder("<auto-complete>", "")
                             .apply {
                                 for ((name, value) in autocompleteResult) {
                                     choice(name, value)
@@ -141,7 +133,7 @@ class KordAutocompleteChecker(val commandManager: CommandManager) {
                     autocompleteExecutor as AutocompleteExecutor<Double>
                     val autocompleteResult = autocompleteExecutor.onAutocomplete(autocompleteContext, focusedCommandOption)
                     bridge.manager.sendNumberAutocomplete(
-                        NumberChoiceBuilder("<auto-complete>", "")
+                        NumberOptionBuilder("<auto-complete>", "")
                             .apply {
                                 for ((name, value) in autocompleteResult) {
                                     choice(name, value)
