1f6502c8092f34501adcf9226150ceb2cd8747fb
MrPowerGamerBR <git@mrpowergamerbr.com>
2022-08-23 13:28:00 -0300

    Update Kord

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
         create("libs") {
             version("discordinteraktions", "0.0.17-SNAPSHOT")
             version("kotlin", "1.7.10")
-            version("kord", "0.8.x-20220720.101712-222")
+            version("kord", "0.8.x-20220821.071910-239")
             alias("kord-common").to("dev.kord", "kord-common").versionRef("kord")
             alias("kord-rest").to("dev.kord", "kord-rest").versionRef("kord")
             alias("kord-gateway").to("dev.kord", "kord-gateway").versionRef("kord")
